f8dd7c89a5b6c12fe422022b58ab52a6cfeabfdc
nadashaban10 <nadashaban10.com>
2023-08-13 16:50:24 -0700

    agaa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* main - entry point 
+ * discription : this c program using puts function to print
+ * return always 0 
+ * \
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
