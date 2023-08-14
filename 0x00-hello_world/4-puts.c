034cf762e5c37c269bcc1f08584e04167b6527c1
nadashaban10 <nadashaban10.com>
2023-08-13 17:08:57 -0700

    first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* main - entry point 
- * discription : this c program using puts function to print
- * return always 0 
- * \
 
-
+/**  
+ *  main - entry point  
+ * Description : A C program that print with puts function
+ * return: always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
