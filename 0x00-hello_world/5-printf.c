c121d2fb5a6b2dc17753f2c89c95a51df0bd817a
nadashaban10 <nadashaban10.com>
2023-08-14 08:53:35 -0700

    sizeof

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: A C program that prints with printf function
- *
- * Return: always 0(success) 
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
