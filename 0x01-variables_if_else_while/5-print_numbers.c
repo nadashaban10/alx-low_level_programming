b495f409a010d52eef211823f70f40cc7d4fb7f9
nadashaban10 <nadashaban10.com>
2023-08-16 15:41:42 -0700

    yee

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int x = 0;
+        
+        while (x <= 10)
+        {
+                printf("%d" ,x);
+                x++;
+        }
+                return (0);
+        }
