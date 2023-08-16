6877927a41701e8e9685e2c63787eaea2e66347c
nadashaban10 <nadashaban10.com>
2023-08-16 16:09:42 -0700

    jjk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int x = 48;
+        
+        while (x <= 57)
+        {
+                putchar(x);
+                x++;
+                
+        }
+        putchar('\n');
+                return (0);
+        }
