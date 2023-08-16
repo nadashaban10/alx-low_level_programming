d96976d46fc5e28423724b651011eebf85a725cf
nadashaban10 <nadashaban10.com>
2023-08-16 07:41:06 -0700

    hqq

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,13 +21,13 @@ int main(void)
 	{
 	    printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (digit < 6)
+	else if (digit == 0)
 	{ 
-	    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	    printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
 	else 
 	{
-	    printf("Last digit of %d is %d and is 0\n", n, digit);
+	    printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
 		return (0);
 }
