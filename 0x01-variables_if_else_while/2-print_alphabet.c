428959f0b77ebfdb9c6714cd0eed02962aad3305
nadashaban10 <nadashaban10.com>
2023-08-16 09:47:55 -0700

    jj

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+/**
+ * main: entry-point
+ * description: print all the alphabet litter 
+ * return 0 always success
+*/
+
+int main(void)
+
+	char ch = 'a';
+	while(ch < = 'z')
+{
+	putchar(ch);
+}
+	ch = ch + 1
+	putchar(\n);
+	return (0);
+}
