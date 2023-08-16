16874652b3202372d1aead1813a2213699e0bd45
nadashaban10 <nadashaban10.com>
2023-08-16 16:30:08 -0700

    aaa

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char x = 'z';
+        
+        while (x >= 'a')
+        {
+                putchar(x);
+                x--;
+                
+        }
+        putchar('\n');
+                return (0);
+        }
