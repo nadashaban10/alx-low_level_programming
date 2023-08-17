43922b8b6fdf8383835608a198d51709eab80976
nadashaban10 <nadashaban10.com>
2023-08-16 18:09:06 -0700

    nna

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *description: print all alphabet letters
+ *
+ *return: always 0 (success)
+ */
+
+int main(void)
+{
+    int x = 0;
+    
+        
+        while (x <= 9)
+        {
+                putchar(x + 48);
+                x++;
+                
+        
+        if (x != 9);
+     {
+         putchar (',');
+         putchar (' ');
+     }
+        }
+                return (0);
+        }
+
