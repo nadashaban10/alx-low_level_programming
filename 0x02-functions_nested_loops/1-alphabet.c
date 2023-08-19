e447ef6fdc738ba011fb429770e18ba63dcb750c
nadashaban10 <nadashaban10.com>
2023-08-19 03:32:35 -0700

    hh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+	int main(void)
+	{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+	putchar (x);
+	x++;
+	}
+	putchar('\n');
+	return (0);
+	}
