4dabb5997ff3607ffdacf0ef5a616557d8245491
nadashaban10 <nadashaban10.com>
2023-08-19 14:49:09 -0700

    ww

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet_x10  - print alphabet 10times
+*return: 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for(x = 0; x <= 10; x++)
+	{
+	for(y = 97; y <=122; y++)
+	_putchar(y);
+	_putchar('\n');
+	}
+}
