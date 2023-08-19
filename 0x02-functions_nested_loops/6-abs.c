ec6d760b408ec8ae6492c61d3363b15a848520be
nadashaban10 <nadashaban10.com>
2023-08-19 16:42:49 -0700

    yyy

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_abs - absulote the sign value of  number.
+*@n: The number to check
+*Return: 0 (success)
+*/
+
+int _abs(int n)
+{
+	n = (-1) * n;
+	_putchar(n);
+	return (0);
+}
