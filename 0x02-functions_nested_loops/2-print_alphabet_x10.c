771d34169514c251531d687e90f09ab14b446459
nadashaban10 <nadashaban10.com>
2023-08-19 14:54:49 -0700

    hhj

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	int x;
 	int y;
 
-	for(x = 0; x < 10; x++)
+	for (x = 0; x < 10; x++)
 	{
-	for(y = 97; y <=122; y++)
+	for (y = 97; y <= 122; y++)
 	_putchar(y);
 	_putchar('\n');
 	}
