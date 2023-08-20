7bf608f7f34ba97aedb5c2bb78b628f1cba2f75f
nadashaban10 <nadashaban10.com>
2023-08-20 09:12:44 -0700

    dnn

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*add - add and sum two integers.
+*@a: first function parameter
+*@b: sec finction parameter
+*Return: the  result of a + b
+*/
+
+int mul(int a, int b)
+{
+        int result = a * b;
+
+        return (result);
+        }
