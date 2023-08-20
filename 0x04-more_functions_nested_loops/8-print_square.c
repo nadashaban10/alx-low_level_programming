656c1fb41c74afa2f694bbf6a21215b25d0978e8
nadashaban10 <nadashaban10.com>
2023-08-20 14:13:50 -0700

    nenen

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,6 +4,7 @@
 /**
 *print_square - print # of the number.
 *@size: The number to print
+* var row and colomn for loop
 */
 
 void print_square(int size)
@@ -12,7 +13,7 @@ void print_square(int size)
 
 	if (size <= 0)
 	_putchar('\n');
-	else 
+	else
 	for (row = 1; row <= size; row++)
 	{
 	for (colomn = 1; colomn <= size; colomn++)
