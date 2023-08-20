8da07528f6e84015dd32b8a0c64d79d5cb033158
nadashaban10 <nadashaban10.com>
2023-08-20 14:00:32 -0700

    doe

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 
+/**
+*print_square - print # of the number.
+*@size: The number to print
+*/
 
 void print_square(int size)
 {
 	int row, colomn;
 
-	for(row = 1; row <= size; row++)
+	for (row = 1; row <= size; row++)
 	{
-	for(colomn = 1; colomn <= size; colomn++)
-	_putchar('#');
+	for (colomn = 1; colomn <= size; colomn++)
+		_putchar('#');
 	_putchar('\n');
 	}
 }
