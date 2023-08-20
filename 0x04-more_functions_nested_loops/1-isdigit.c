863f9e557a4fd683df65349f99b443ef45ba77d2
nadashaban10 <nadashaban10.com>
2023-08-20 08:58:57 -0700

    donnee

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+*_isdigit  - checks the digit number.
+*@c: The number to check
+*Return: (1) for digits
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	return (1);
+	else
+	return (0);
+}
