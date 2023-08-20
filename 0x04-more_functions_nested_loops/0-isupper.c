6bdb1f49818d66e429293d15ee1b2d1fb4a85fe8
nadashaban10 <nadashaban10.com>
2023-08-20 08:21:01 -0700

    haw

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+*_isupper  - checks the upper case letter.
+*@c: The number to check letter
+*Return:(1) if uppercase (0) for else
+*/
+
+
+int _isupper(int c)
+{
+	if (c <= 65 && c >= 90)
+	return (1);
+	else
+	return (0);
+}
