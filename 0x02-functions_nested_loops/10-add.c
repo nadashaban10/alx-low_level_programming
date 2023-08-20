fb9210aeb66bee0c7e3afec33530c2432fa28a5a
nadashaban10 <nadashaban10.com>
2023-08-20 07:33:46 -0700

    hha

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*add - add and sum two integers.
+*@a first function parameter
+*@b sec finction parameter
+*Return:addition result
+*/
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+	}
+
