ebe09d432d5f78642fa554a12d7706a90b87a1e6
nadashaban10 <nadashaban10.com>
2023-08-20 07:35:47 -0700

    hha

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 
 /**
 *add - add and sum two integers.
-*@a first function parameter
-*@b sec finction parameter
-*Return:addition result
+*@a: first function parameter
+*@b: sec finction parameter
+*Return: the  result of a + b
 */
 
 int add(int a, int b)
