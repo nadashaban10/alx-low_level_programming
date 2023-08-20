b76b6762000494e7ddb08aebf0028d5d3ad55939
nadashaban10 <nadashaban10.com>
2023-08-20 09:35:38 -0700

    hhaa

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+*print_numbers - print numbers from 0 to 9 using _putchar funtcion
+*Return: 0 always (success)
+*/
+
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+{
+	_putchar (c);
+	_putchar('\n');
+	c++;
+}
+}
