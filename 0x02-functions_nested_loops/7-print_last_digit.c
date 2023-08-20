1baf7fb22b01d4bc5f7a017cbbed9e2e6105aa1a
nadashaban10 <nadashaban10.com>
2023-08-19 17:38:22 -0700

    hhaaaaa

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+
+nLength - checks the length of the number.
+@n: The number to check
+*
+Return: digits number
+*/
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+	if (n > 0)
+	_putchar(last + '0');
+	else 
+		_putchar( -1 * n % 10)
+			return (n);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+*jack_bauer - print every mins on 24 hours
+*/
+
+void jack_bauer(void)
+{
+	int hr ;
+	int mins ;
+	for (hr = 0; hr <= 23; hr++)
+	{
+	for (mins = 0; mins <= 59; mins++)
+	{
+	_putchar(hr / 10 + 48);
+	_putchar(hr % 10 + 48);
+	_putchar(':');
+	_putchar(mins / 10 + 48);
+	_putchar(mins % 10 + 48);
+	_putchar('\n');
+	}
+	}
+}
