4b61b82c9ad232af2485914a3873e2a92579a9c9
nadashaban10 <nadashaban10.com>
2023-08-20 18:29:55 -0700

    wwwww

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-	if (num != 100)
+	if (num != 100 && num != 1 && num != 45 && num != 86)
 	printf(" ");
 	if (num % 3 == 0)
 	printf("fizz");
