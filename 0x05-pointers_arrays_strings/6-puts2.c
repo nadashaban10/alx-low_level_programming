a3262851742765dc2219262db46cd1c88a714e3d
nadashaban10 <nadashaban10.com>
2023-08-22 05:36:04 -0700

    dnee

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*puts2 - prints one char out of 2 of a string
+*followed by a new line
+*@str: string to print the chars from
+*/
+
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+	if (str[i] % 2 == 0)
+	_putchar(str[i]);
+}
+	_putchar('\n');
+}
