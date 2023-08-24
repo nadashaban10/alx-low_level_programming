079bccc8ce6684e8111fa76eda9c0f2b2cd0bcc7
nadashaban10 <nadashaban10.com>
2023-08-24 15:27:22 -0700

    comparing two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,17 +6,36 @@
  * @s2: second string to compare
  * Return:  0< if s1 < s2, >0 if s1 > s2, 0 if s1 == s2 
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	int total = 0;
 	{
-	if (*s1 == '\0')
-	{
-	return (0);
-	}
+	while (*s1)
+{
+	if ( *s1 != *s2)
+	total = ((int)*s1 - 48) - ((int)*s2 - 48);
+	break; 
+}
 	s1++;
 	s2++;
 	}
-	return (*s1 - *s2);
+	return (total);
 }
+
+
+/**int _strcmp(char *s1, char *s2)
+*{
+*	while (*s1 == *s2)
+*	{
+*	if (*s1 == '\0')
+*	{
+*	return (0);
+*	}
+*	s1++;
+*	s2++;
+*	}
+*	return (*s1 - *s2);
+*	}
+*/
 
