6e668c2039f53d543106484fd75741a49123bbb1
nadashaban10 <nadashaban10.com>
2023-08-24 09:00:21 -0700

    return dest using strcat func

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
 * the pointers stored a string value
 * @dest: pointer to the print variable
 *@src: pointer to print
-*return: (dest) appendding src
+*Return: _strcat (dest) resulting string
 */
 
 char *_strcat(char *dest, char *src)
