ebe0393ad3332738c507b748f6d073cd3a495cd6
nadashaban10 <nadashaban10.com>
2023-08-24 08:54:42 -0700

    return dest using strcat func

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-*_strcat - write to append src str in dest
+*_strcat - function that concatenates two strings.
 * the pointers stored a string value
 * @dest: pointer to the print variable
-*@src: pointer to print 
+*@src: pointer to print
+*return: (dest) appendding src
 */
 
 char *_strcat(char *dest, char *src)
 {
 	int x = 0;
 	int y;
-	
+
 	while (dest[x])
-		x++;
+	x++;
 	for (y = 0; src[y]; y++)
 	dest[x++] = src[y];
 	return (dest);
