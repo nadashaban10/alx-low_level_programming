5cf44c5467f9d9289d9d15b5ef638cec6f0cc4a6
nadashaban10 <nadashaban10.com>
2023-08-24 15:41:25 -0700

    func for comparing two str

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,41 +1,25 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
- * @s1: first string to compare
- * @s2: second string to compare
- * Return:  0< if s1 < s2, >0 if s1 > s2, 0 if s1 == s2 
+* _strcmp - compares two strings
+* @s1: first string to compare
+* @s2: second string to compare
+* Return:total value  0< if s1 < s2, >0 if s1 > s2, 0 if s1 == s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int total = 0;
+
 	{
 	while (*s1)
 {
-	if ( *s1 != *s2)
+	if (*s1 != *s2)
 	total = ((int)*s1 - 48) - ((int)*s2 - 48);
-	break; 
+	break;
 }
 	s1++;
 	s2++;
 	}
 	return (total);
 }
-
-
-/**int _strcmp(char *s1, char *s2)
-*{
-*	while (*s1 == *s2)
-*	{
-*	if (*s1 == '\0')
-*	{
-*	return (0);
-*	}
-*	s1++;
-*	s2++;
-*	}
-*	return (*s1 - *s2);
-*	}
-*/
-
