54c227da9f07560b61dfbafc9830efc1a16f3625
nadashaban10 <nadashaban10.com>
2023-08-24 08:29:48 -0700

    dn

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strcat - write to append src str in dest
+* the pointers stored a string value
+* @dest: pointer to the print variable
+*@src: pointer to print 
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int x = 0;
+	int y;
+	
+	while (dest[x])
+		x++;
+	for (y = 0; src[y]; y++)
+	dest[x++] = src[y];
+	return (dest);
+}
