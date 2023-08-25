03c8bda4b0d58b6b2c95fd71b626d48a29c06dfe
nadashaban10 <nadashaban10.com>
2023-08-24 19:00:07 -0700

    ssa

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+*cap_string: function that capitalizes all words of a string.
+*@s: string to modify
+*Return: the resulting string
+*/
+
+
+char *cap_string(char *s)
+{
+	int index = 0;
+	
+	while (s[index])
+	{
+	while (s[index] >= 'a' && s[index] <= 'z')
+	index++;
+	if (s[index - 1] == ',' ||
+       	s[index - 1] == ';' ||
+       	s[index - 1] == '\n' ||
+	s[index - 1] == '.' ||
+       	s[index - 1] == '!' ||
+	s[index - 1] == '?' || 
+	s[index - 1] == '"' ||
+       	s[index - 1] == ')' ||
+       	s[index - 1] == '(' ||
+       	s[index - 1] == '}' || 
+	s[index - 1] == '{' ||
+       	s[index - 1] == ' ' ||
+       	s[index - 1] == '\t' ||
+        index == 0)
+	s[index]-= 32;
+	index++;
+	}
+	return (s);
+}
