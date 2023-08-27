30192f1ad9bcfff595ab4ef00f02daa6f4a5bb15
nadashaban10 <nadashaban10.com>
2023-08-27 04:25:38 -0700

    prints alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-#include <time.h>
+
+
 /**
-*main - entry point
-*description:  a program that prints the alphabet in lowercase
-*Return: Always 0 (Success)
-*/
+ *main - entry point
+ *description:  a program that prints the alphabet in lowercase
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 	int i;
-	
+
 	for (i = 97; i <= 122; i++)
 	{
-	if ( i != 101 && i != 113)
-	{
-	putchar(i);
-	i++;
-	}
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
