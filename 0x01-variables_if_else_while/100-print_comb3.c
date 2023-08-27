d17f8f18e1486df82bc4a4a4e6f09bf50ff34090
nadashaban10 <nadashaban10.com>
2023-08-27 06:11:10 -0700

    haaa

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+
+			putchar(',');
+			putchar(' ');
+
+			putchar(j + '0');
+		}
+	}
+
+	putchar('\n');
+	return 0;
+}
