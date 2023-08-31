033b723bc428cb71e54a22d3f2f93d2a69efd913
nadashaban10 <nadashaban10.com>
2023-08-30 17:33:56 -0700

    print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include "unistd.h"
+
+/**
+ *_puts_recursion - funtcion print strings
+ *@s: input string
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
