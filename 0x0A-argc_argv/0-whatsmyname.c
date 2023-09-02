ce0cf9872ac472996010bd49a25db72da0d07994
nadashaban10 <nadashaban10.com>
2023-09-02 15:12:22 -0700

    argcargv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point program print
+ *@argv : array string
+ *@argc : count element
+ *Return : (0)
+ * */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
