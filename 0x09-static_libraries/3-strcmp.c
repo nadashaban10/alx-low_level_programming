03095e1b5ce378865e3296adbb1ac1d1fdf37d03
nadashaban10 <nadashaban10.com>
2023-09-01 20:13:32 -0700

    library

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: first string to compare
+* @s2: second string to compare
+* Return:total value  0< if s1 < s2, >0 if s1 > s2, 0 if s1 == s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int total = 0;
+
+	{
+	while (*s1)
+{
+	if (*s1 != *s2)
+	total = ((int)*s1 - 48) - ((int)*s2 - 48);
+	break;
+}
+	s1++;
+	s2++;
+	}
+	return (total);
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_strpbrk - function that searches a string for any of a set of byte
+ *
+ *@s: sring whitch search in
+ *@accept: we locate for
+ *Return: (&s[i])
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s != '\0')
+	{
+		s++;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+
+			return (s);
+		}
+	
+	}
+
+	return (0);
+}
