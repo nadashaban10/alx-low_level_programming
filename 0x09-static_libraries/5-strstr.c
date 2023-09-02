ecead3221752998ec2d19e1457d1f0d6cf6bf8f8
nadashaban10 <nadashaban10.com>
2023-09-01 19:31:46 -0700

    aa

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *_strstr -  function that locates a substring.
+ *@haystack: search in
+ *@needle: search for
+ *Return: (0 for null or p for values)
+ */
+#include <stdio.h>
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j;
+	char *p;
+
+	if (*needle == 0)
+		return (haystack);
+	while (needle[i] != '\0')
+		i++;
+
+	while (*haystack != '\0')
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				p = (needle);
+				return (p);
+			}
+		}
+		haystack++;
+	}
+
+	return (0);
+}
