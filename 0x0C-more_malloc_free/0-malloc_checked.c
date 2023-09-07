6d3a372ea9538ef25c09099bfc72bcd9f4892088
nadashaban10 <nadashaban10.com>
2023-09-07 16:51:12 -0700

    aa

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*
+*
+*
+*/
+void *malloc_checked(unsigned int b)
+{
+char *ptr;
+
+ptr = (char*)malloc(b * sizeof(unsigned int));
+        if (ptr == NULL)
+        {
+                exit(98);
+        }
+        return (ptr);
+}
