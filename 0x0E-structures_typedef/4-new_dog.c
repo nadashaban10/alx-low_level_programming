7f6bd215a545257edeacce693266f88b757fd48d
nadashaban10 <nadashaban10.com>
2023-09-11 15:22:44 -0700

    haa

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,40 @@
 #include <stdlib.h>
 #include "dog.h"
 #include <string.h>
-dog_t *new_dog(char *name, float age, char *owner) {
-    /*Allocate memory for the dog structure*/
-    dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
-    /*condition if failed*/
-    if (newDog == NULL) {
-        return NULL; 
-    }
-    
-    /*Allocate memory for the name w owner and copy the values*/
-    newDog->name = (char *)malloc(strlen(name) + 1);
-    newDog->owner = (char *)malloc(strlen(owner) + 1);
-    
-    if (newDog->name == NULL || newDog->owner == NULL) {
-        /*Memory allocation if name or owner failed*/
-        free(newDog->name);
-        free(newDog->owner);
-        free(newDog);
-        return NULL;
-    }
-    
-    strcpy(newDog->name, name);
-    newDog->age = age;
-    strcpy(newDog->owner, owner);
-    
-    return newDog;
+
+/**
+ *new_dog - function that creates a new dog.
+ *@name: dogs name
+ *@owner: dogs owner
+ *@age: dogs age
+ *Return: pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	/*Allocate memory for the dog structure*/
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+	/*condition if failed*/
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+
+	/*Allocate memory for the name w owner and copy the values*/
+	newDog->name = (char *)malloc(strlen(name) + 1);
+	newDog->owner = (char *)malloc(strlen(owner) + 1);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		/*Memory allocation if name or owner failed*/
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+
+	strcpy(newDog->name, name);
+	newDog->age = age;
+	strcpy(newDog->owner, owner);
+
+	return (newDog);
 }
