23da56ed05f9c68c5ee71790b14b4b40aeac47e1
nadashabn10 <nadashaban10@github.com>
2023-09-21 16:52:17 -0700

    zero

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,14 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-	if (h->str == NULL)
-		printf("[0] (nill)\n");
-	else
-		printf("[%d] %s\n", h->len, h->str);
-			/*here we have to add len snd str var in structure*/
-	nodes++;	/*this iterate to cound the nodes number */
-	h = h->move;	/*h will equal the value of next move and so on..*/
-			/*this move ptr in struct to access*/
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		/*here we have to add len snd str var in structure*/
+		nodes++;        /*this iterate to cound the nodes number */
+		h = h->next;    /*h will equal the value of next move and so on..*/
+		/*this move ptr in struct to access*/
 	}
-return (nodes);
+	return (nodes);
 }
-
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,11 @@
-#ifndef STRC_H
-#define STRC_H
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
-typedef struct list_s 
+typedef struct list_s
 {
-	char *str;
-	int len;
-	struct list_s *move;
+        char *str;
+        int len;
+        struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
