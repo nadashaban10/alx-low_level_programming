3801d20b3485b5242a2eef1807649148a4d57e5a
nadashabn10 <nadashaban10@github.com>
2023-09-21 18:45:16 -0700

    done zero

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+#include <stdio.h>
+typedef struct list_s {
+	char *str;          
+	int len;           
+	struct list_s *next;  
+} list_t;
+
+
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
