ff27318bf8bc9624bb691c4d4671a6e8b1586acf
nadashaban10 <nadashapann@gmail.com>
2023-11-09 15:27:47 -0800

    dn

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,7 +9,6 @@
  * Return: the address of the new element
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-<<<<<<< HEAD
 
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
@@ -34,33 +33,4 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->prev = current_node;
 
 	return (new_node);
-=======
-{
-	dlistint_t *h;
-	dlistint_t *new;
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	h = *head;
-
-	if (h != NULL)
-	{
-		while (h->next != NULL)
-			h = h->next;
-		h->next = new;
-	}
-	else
-	{
-		*head = new;
-	}
-
-	new->prev = h;
-
-	return (new);
->>>>>>> 43bd807ae97282223f88109c598ce4b9bb9000ab
 }
