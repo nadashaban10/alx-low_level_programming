fcdcc953d228213c38ff0c650681143226a5b6fc
nadashaban10 <nadashapann@gmail.com>
2023-11-09 16:23:48 -0800

    dann

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,54 +1,52 @@
 #include "lists.h"
+
 /**
- *delete_dnodeint_at_index - delete function
- *@head: headofndes
- *@index: index node
- *Return: reaturn value
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
+ *
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) 
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *h1;
+	dlistint_t *h2;
 	unsigned int i;
 
-	if (*head == NULL) 
-	{
-		return (-1);
-	}
+	h1 = *head;
 
-	if (index == 0) 
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		*head = (*head)->next;
-		if (*head != NULL) 
+		if (i == index)
 		{
-			(*head)->prev = NULL;
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
+			return (1);
 		}
-		return (1);
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
 
-	dlistint_t *current = *head;
-
-	for (i = 0; i < index - 1; i++) 
-	{
-		if (current->next == NULL) 
-		{
-			return (-1);
-		}
-		current = current->next;
-	}
-
-	dlistint_t *node_to_delete = current->next;
-	
-	if (node_to_delete == NULL)
-	{
-		return (-1);
-	}
-
-	current->next = node_to_delete->next;
-	if (node_to_delete->next != NULL) 
-	{
-		node_to_delete->next->prev = current;
-	}
-
-	free(node_to_delete);
-
-	return (1);
+	return (-1);
 }
