82cd20932cefdca0d46b422d4227819a957e0b8e
nadashaban10 <nadashaban10@github.com>
2023-12-02 01:05:00 +0200

    dn

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ *hash_djb2 - function used in hash table
+ *@str: string given
+ *Return: hash table
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
+
