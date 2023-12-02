cc3d5c1593d9a5df97fa4cbaf12b3fec49d70d3e
nadashaban10 <nadashaban10@github.com>
2023-12-02 04:11:55 +0200

    delet ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_delete - Deletes a hash table.
  * @ht: A pointer to a hash table.
@@ -13,17 +12,17 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
-		{
+	{
 			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
-		}
+	while (node != NULL)
+	{
+	tmp = node->next;
+	free(node->key);
+	free(node->value);
+	free(node);
+	node = tmp;
+	}
+	}
 	}
 	free(head->array);
 	free(head);
